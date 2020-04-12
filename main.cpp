38d2e400d802a846beee84a5513a17a47eaa78ce
ho4u <kulikov87dev@gmail.com>
2020-04-13 00:42:57 +0300

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 int main()
 {
 	std::cout << "Hello world" << std::endl;
-	std::cout << "Enter x: ";
-	int x;
-	std::cin >> x;
+	std::cout << "Enter x and y: ";
+	int x, y;
+	std::cin >> x >> y;
+	std::cout << "Sum of " << x << " + " << y << " = " << x + y << std::endl;
+	
 	return 0;
 }
