ed4b48576f4f6b7189bb05bc4a8f6197020de4b1
ho4u <kulikov87dev@gmail.com>
2020-05-04 22:13:32 +0300

    add some tests

--- test.cpp
+++ test.cpp
@@ -3,7 +3,9 @@
 #include "mysum.hpp"
 
 TEST_CASE( "mysum computed", "[factorial]" ) {
-    REQUIRE( mysum(1, 0) == 2 );
+    REQUIRE( mysum(1, 0) == 1);
     REQUIRE( mysum(3, -15) == -12 );
     REQUIRE( mysum(20, 15) == 35 );
+    REQUIRE( mysum(-10, 10) == 0 );
+    REQUIRE( mysum(-25, -15) == -40 );
 }
